c180f6ce59b93be44165e1ca343ba4e19829dbc0
francesschisom <chisomonwuzulike@gmail.com>
2023-02-19 08:49:40 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdilib.h>
+#include <time.h>
+/**
+ * main - main block
+ * Description: get a random lt with 5
+ * Return: 0
+ */
+int main(void)
+{
+
+
+
+	char c = 'a';
+
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+
+	putchar('\n');
+	return (0);
+}
+
